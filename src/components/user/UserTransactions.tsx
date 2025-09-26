c9a857a9d62c6313b51a38024df41b32c50bc8b5
George <nabuage@outlook.com>
2025-09-26 18:21:35 -0500

    Use data table.

--- src/components/user/UserTransactions.tsx
+++ src/components/user/UserTransactions.tsx
@@ -1,8 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { API, type UserTransaction } from "../utils/API";
-import { useMemo, useState } from "react";
-import { DataTable } from "../generic/DataTable";
-import type { MRT_ColumnDef } from "material-react-table";
+import { useState } from "react";
 import { LocalDate } from "@js-joda/core";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
@@ -11,6 +9,8 @@ import dayjs from "dayjs";
 import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
 import { PageHeader } from "../generic/PageHeader";
+import type { ColumnDefinition } from "../generic/table/GenericTableHead";
+import DataTable from "../generic/table/DataTable";
 
 export const UserTransactions = () => {
   const [year, setYear] = useState<number>(LocalDate.now().year());
@@ -20,41 +20,6 @@ export const UserTransactions = () => {
     queryFn: () => API.getUserTransaction(year, month)
   });
 
-  const totalAmount = useMemo(() => {
-      if (data == null || data?.length == 0) {
-        return null;
-      }
-
-      return data.reduce((acc, curr) => acc + curr.amount, 0);
-    },
-    [data]
-  );
-
-  const columns = useMemo<MRT_ColumnDef<UserTransaction>[]>(
-    () => [
-      {
-        accessorKey: "merchantName",
-        header: "Merchant",
-      },
-      {
-        accessorKey: "transactionDate",
-        header: "Transaction Date",
-      },
-      {
-        accessorKey: "amount",
-        header: "Amount",
-        Footer: () => (
-          <>
-            {totalAmount != null &&
-              <>Total: {totalAmount.toFixed(2)}</>
-            }
-          </>
-        )
-      }
-    ],
-    [totalAmount],
-  );
-
   const handleDateChange = (value: dayjs.Dayjs | null, context: { validationError: string | null }) => {
     if (!context.validationError) {
       if (value !== null) {
@@ -63,6 +28,30 @@ export const UserTransactions = () => {
       }
     }
   }
+
+  const userColumns: ColumnDefinition<UserTransaction>[] = [
+    {
+      title: 'Merchant', accessorKey: 'merchantName',
+      id: "merchantName",
+      numeric: false,
+      disablePadding: false,
+      label: 'Merchant'
+    },
+    {
+      title: 'Date', accessorKey: 'transactionDate',
+      id: "transactionDate",
+      numeric: false,
+      disablePadding: false,
+      label: 'Transaction Date'
+    },
+    {
+      title: 'Amount', accessorKey: 'amount',
+      id: "amount",
+      numeric: false,
+      disablePadding: false,
+      label: 'Amount'
+    },
+  ];
 
   return (
     <>
@@ -82,8 +71,8 @@ export const UserTransactions = () => {
               views={["month", "year"]}
               onAccept={handleDateChange} />
           </LocalizationProvider>        
-        </Box>        
-        <DataTable columns={columns} data={data ?? []} />
+        </Box>
+        <DataTable data={data ?? []} columns={userColumns} columnId="transactionId" select={false} title=""/>
       </Stack>      
     </>
   );
